3f600b67c47d1297ff2f4650ae86f8af81a353d3
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2013-08-21 16:47:18 +0100

    Fix auto-accept license

--- recipes/ppa.rb
+++ recipes/ppa.rb
@@ -6,13 +6,3 @@ apt_repository "oracle-java-6" do
   key "EEA14886"
   action :add
 end
-
-execute "accept-oracle-license" do
-  command "echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true"
-  action :run
-end
-
-execute "deconf-set-selections" do
-  command "/usr/bin/debconf-set-selections"
-  action :run
-end
