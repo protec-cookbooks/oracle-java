12d4a08c7d5fd3aace1bf7593752ae7bb8d63f66
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2013-08-21 16:55:22 +0100

    fix license accepting

--- recipes/java6.rb
+++ recipes/java6.rb
@@ -1,12 +1,7 @@
 include_recipe 'oracle-java::ppa'
 
 execute "accept-oracle-license" do
-  command "echo oracle-java6-installer shared/accepted-oracle-license-v1-1 select true"
-  action :run
-end
-
-execute "deconf-set-selections" do
-  command "/usr/bin/debconf-set-selections"
+  command "echo oracle-java6-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
   action :run
 end
 
